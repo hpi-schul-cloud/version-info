b3bcbd78c6fa5d491d54a5f1958688e4cb9f212f
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-07-15 14:22:24 +0200

    filter fields

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const getRepoInfo = require('git-repo-info');
-const program = require('commander');
 const fs = require('fs');
 
-program
-  .option('f', '--file', 'output filename')
-  .parse(process.argv);
+const fields = [
+  'abbreviatedSha',
+  'sha',
+  'branch',
+  'tag',
+  'lastTag',
+  'committerDate',
+  'commitsSinceLastTag',
+  'commitMessage',
+];
 
 const run = () => {
   const pkg = JSON.parse(fs.readFileSync('package.json'));
+
   const info = getRepoInfo();
+
   const { version } = pkg;
-  info.version = version;
-  if (program.file) {
-    fs.writeFileSync(program.file, JSON.stringify(info));
-  } else {
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(info));
-  }
+
+  const result = { };
+  result.version = version;
+  fields.forEach((field) => {
+    result[field] = info[field];
+  });
+
+  // eslint-disable-next-line no-console
+  console.log(JSON.stringify(result));
 };
 
 run();
